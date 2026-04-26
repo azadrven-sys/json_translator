18af5b8cfb7131c6ed0dff06d3ba10e0a19e9976
azadrven-sys <aza.d.r.ven@gmail.com>
2026-04-26 22:02:03 +0300

    Update json_translator.bat

--- json_translator.bat
+++ json_translator.bat
@@ -1,1 +1,1 @@
-powershell -NoProfile -ExecutionPolicy Bypass -File json_translator.ps1 -help tr -InputDirectory .\* -OutputDirectory .\ -InputLanguage en -OutputLanguage en -SkipOnError
+powershell -NoProfile -ExecutionPolicy Bypass -File json_translator.ps1 -help tr -InputDirectory .\* -OutputDirectory .\result -InputLanguage en -OutputLanguage tr -SkipOnError
